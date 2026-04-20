5e98a1005ae8e919b48da8d0c16817d8346249a6
DStacyy <gabutajajirlah@gmail.com>
2026-04-20 11:10:22 +0700

    first

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'db_gallery';
+
+$conn = 'new mysqli($host, $user, $pass, $db)';
+if ($conn->connect_error){
+    die("koneksi gagal cik: " . $conn->connect_error);
+}
